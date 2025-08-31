bb9dc4a9e13daf6bcf5a3ad9c31eb53f08d42f7a
Ong Hua Yu <onghuayu5b@gmail.com>
2025-08-31 21:46:36 +0800

    Scrollbar List Update

--- app/components/RestaurantList.tsx
+++ app/components/RestaurantList.tsx
@@ -9,7 +9,7 @@ export const RestaurantList: React.FC<Props> = ({ restaurants }) => {
   return (
     <div className="restaurant-list">
       {restaurants.map((r, i) => (
-        <h1 key={i}>{r.name}</h1>
+        <h1 key={i} className={`restaurant-name ${i%2==0 ? "restaurant-name-even" : "restaurant-name-odd"}`}>{r.name}</h1>
       ))}
     </div>
   )
